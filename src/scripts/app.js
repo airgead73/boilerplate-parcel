8460f22271e0cec231b0628cceec8c5d0df2db39
Dan Moneypenny <catullus1973@gmail.com>
2022-01-22 06:25:01 -0800

    update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,16 @@
 console.log('script is loaded');
+
+const fetchAuthors = async () => {
+  const response = await fetch("http://localhost:5000/authors");
+  const data = await response.json();
+  console.log("authors", data);
+}
+
+const fetchBooks = async () => {
+  const response = await fetch("http://localhost:5000/books");
+  const data = await response.json();
+  console.log("books", data);
+}
+
+fetchAuthors();
+fetchBooks();
